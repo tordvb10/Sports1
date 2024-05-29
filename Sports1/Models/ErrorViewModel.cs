39b4ba6190f8f6ec9365cffc18986627bba67838
tordvb10 <tordvb10.kodehode@gmail.com>
2024-05-29 15:05:56 +0200

    Add project files.

---
+++ Sports1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sports1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Sports1/Models/SportsModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Sports1.Models
+{
+    public class SportsModel
+    {
+        public string ID { get; set; } 
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int IsActive { get; set; }
+
+    }
+}
